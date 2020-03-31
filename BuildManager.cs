3c8162c189c449b3d0a3f7a2369cfc027f3452d0
KapitonasDzekas <61228694+KapitonasDzekas@users.noreply.github.com>
2020-03-31 15:53:09 +0300

    Add files via upload

---
+++ BuildManager.cs
@@ -0,0 +1,55 @@
+﻿using UnityEngine;
+
+public class BuildManager : MonoBehaviour
+{
+    public static BuildManager instance;
+
+    void Awake()
+    {
+        if (instance != null)
+        {
+            Debug.LogError("More than one BuildManager in scene!");
+            return;
+        }
+        instance = this;
+    }
+
+    public GameObject standartTurretPrefab;
+    public GameObject missileLauncherPrefab;
+
+    public GameObject buildEffect;
+
+    private TurretBlueprint turretToBuild;
+
+    public bool CanBuild
+    {
+        get { return turretToBuild != null; }
+    }
+    public bool HasMoney
+    {
+        get { return PlayerStats.Money >= turretToBuild.cost; }
+    }
+
+    public void BuildTurretOn(Node node)
+    {
+        if(PlayerStats.Money < turretToBuild.cost)
+        {
+            Debug.Log("Not enough money");
+            return;
+        }
+        PlayerStats.Money -= turretToBuild.cost;
+
+        GameObject turret = (GameObject)Instantiate(turretToBuild.prefab, node.GetBuildPosition(), Quaternion.identity);
+        node.turret = turret;
+
+        GameObject effect = (GameObject)Instantiate(buildEffect, node.GetBuildPosition(), Quaternion.identity);
+        Destroy(effect, 5f);
+
+        Debug.Log("Turret build! Money left: " + PlayerStats.Money);
+    }
+
+    public void SelectTurretToBuild(TurretBlueprint turret)
+    {
+        turretToBuild = turret;
+    }
+}
