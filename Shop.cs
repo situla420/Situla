654aab86fbdd0a08b8df735383f5ed057915a5be
KapitonasDzekas <61228694+KapitonasDzekas@users.noreply.github.com>
2020-03-31 15:54:24 +0300

    Add files via upload

---
+++ Shop.cs
@@ -0,0 +1,27 @@
+﻿using UnityEngine;
+
+public class Shop : MonoBehaviour
+{
+    public TurretBlueprint standartTurret;
+    public TurretBlueprint missileLauncher;
+
+    BuildManager buildManager;
+
+    void Start()
+    {
+        buildManager = BuildManager.instance;
+    }
+
+    public void SelectStandartTurret()
+    {
+        Debug.Log("Standart turret selected");
+        buildManager.SelectTurretToBuild(standartTurret);
+
+    }
+
+    public void SelectMissileLauncher()
+    {
+        Debug.Log("Missile launcher selected");
+        buildManager.SelectTurretToBuild(missileLauncher);
+    }
+}
